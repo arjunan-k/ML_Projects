33334e72ab40db1d9086213bd3ba4073c07a5ad8
Arjunan K <arjunank680620@gmail.com>
2023-04-23 00:14:06 +0530

    update

---
+++ React/Task Custom Hook/src/App.js
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from 'react';
+import Tasks from './components/Tasks/Tasks';
+import NewTask from './components/NewTask/NewTask';
+import useHttp from './hooks/use-http';
+
+const URL = "Firebase Url"
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+
+  // const transformTasks = tasksObj => {
+  //   const loadedTasks = [];
+
+  //   for (const taskKey in tasksObj) {
+  //     loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
+  //   }
+
+  //   setTasks(loadedTasks);
+  // }
+
+  const httpData = useHttp()
+  const {isLoading, error, sendRequest: fetchTasks} = httpData
+
+  useEffect(() => {
+    const transformTasks = tasksObj => {
+      const loadedTasks = [];
+  
+      for (const taskKey in tasksObj) {
+        loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
+      }
+  
+      setTasks(loadedTasks);
+    }
+
+    fetchTasks({url: URL}, transformTasks);
+  }, []);
+
+  const taskAddHandler = (task) => {
+    setTasks((prevTasks) => prevTasks.concat(task));
+  };
+
+  return (
+    <React.Fragment>
+      <NewTask onAddTask={taskAddHandler} />
+      <Tasks
+        items={tasks}
+        loading={isLoading}
+        error={error}
+        onFetch={fetchTasks}
+      />
+    </React.Fragment>
+  );
+}
+
+export default App;
